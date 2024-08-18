649bfb1102e056fc2d11ed871defd2617f731fb9
Rodolfo <resgem585@gmail.com>
2024-08-17 20:42:04 -0600

    se agrego login para user

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,8 +1,24 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 const Query = {
   users: async () => {
     return await User.find({});
+  },
+
+  login: async (_, { email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password');
+    }
+
+    // Retornar el usuario si la autenticación es exitosa
+    return user;
   },
 };
 
