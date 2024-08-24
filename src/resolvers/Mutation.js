e67d29149c4555541e77761305460c323c4a8b0c
Rodolfo <resgem585@gmail.com>
2024-08-24 16:29:03 -0600

    se agrego crud completo a user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -4,11 +4,24 @@ import User from '../models/User.js';
 
 const Mutation = {
     // USER
-    async createUser(_, { email, password }) {
-        const newUser = { email, password };
-        const user = await User.create(newUser);
-        return user; // Regresa el usuario recién creado
-    },
-};
+   // USER
+   async createUser(_, {email, password }){
+    const newUser = { email, password}
+    const user = await User.create( newUser )
+    return await User.find() // puede ser solo user, regresar el usuario.
+},
+async updateUser(_, { _id, email, password } ){
+    return await User.findByIdAndUpdate(_id, { email, password, }, {new: true})
+},
+async deleteUser( _, {_id}){
+    const user = await User.findByIdAndDelete(_id);
+    if(!user){
+        
+     throw new Error('User not found');
+    }
+    return user
+
+}
+}
 
 export default Mutation;
