8a87a4eb54c6ed1ca6e8526cdef712da43f9fe4a
Rodolfo <resgem585@gmail.com>
2024-09-04 19:19:00 -0600

    se modifica id para donantes

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -14,15 +14,29 @@ async login(_,{email,password} ) {
      return verifyUser
 },
 
-donantes: async () => {
-  return await Donante.find({});
+async donantes() {
+  try {
+    const donantes = await Donante.find({});
+    return donantes;
+  } catch (error) {
+    throw new Error("Error al obtener donantes");
+  }
 },
 
-donante: async (_, { id }) => {
-  return await Donante.findById(id);
-},
-
+// Resolver para obtener un donante por ID
+async donante(_, { id }) {
+  try {
+    const donante = await Donante.findById(id);
+    if (!donante) {
+      throw new Error('Donante no encontrado');
+    }
+    return donante;
+  } catch (error) {
+    throw new Error('Error al obtener el donante');
+  }
+}
 };
+
 
 export default Query;
 
