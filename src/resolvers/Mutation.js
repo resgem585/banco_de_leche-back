c5d998b155e1dc22d5333009fcf79aefcb03b22e
Rodolfo <resgem585@gmail.com>
2024-09-07 14:02:31 -0600

    se arreglo update donante

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,29 +1,28 @@
-
 import User from '../models/User.js';
 import Donante from '../models/Donante.js'; 
 
-
 const Mutation = {
-   // USER
-   async createUser(_, {email, password }){
-    const newUser = { email, password}
-    const user = await User.create( newUser )
-    return await User.find() // puede ser solo user, regresar el usuario.
-},
-async updateUser(_, { _id, email, password } ){
-    return await User.findByIdAndUpdate(_id, { email, password, }, {new: true})
-},
-async deleteUser( _, {_id}){
-    const user = await User.findByIdAndDelete(_id);
-    if(!user){
-        
-     throw new Error('User not found');
-    }
-    return user
+  // USER
+  async createUser(_, { email, password }) {
+    const newUser = { email, password };
+    const user = await User.create(newUser);
+    return await User.find(); // puede ser solo user, regresar el usuario.
+  },
 
-},
- // Donante
-createDonante: async (_, { input }) => {
+  async updateUser(_, { _id, email, password }) {
+    return await User.findByIdAndUpdate(_id, { email, password }, { new: true });
+  },
+
+  async deleteUser(_, { _id }) {
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
+  },
+
+  // Donante
+  createDonante: async (_, { input }) => {
     const donante = new Donante(input);
     await donante.save();
     return donante;
@@ -34,10 +33,13 @@ createDonante: async (_, { input }) => {
     return donante;
   },
 
-  deleteDonante: async (_, { id }) => {
-    await Donante.findByIdAndDelete(id);
+  deleteDonante: async (_, { _id }) => {
+    const donante = await Donante.findByIdAndDelete(_id); // Cambié `id` por `_id`
+    if (!donante) {
+      throw new Error('Donante no encontrado');
+    }
     return "Donante eliminado con éxito";
   },
-}
+};
 
 export default Mutation;
