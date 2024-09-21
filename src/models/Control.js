fa6d13e89639ec9a8db784563fa0325e6ce1cc53
Rodolfo <resgem585@gmail.com>
2024-09-20 20:04:13 -0600

    ya sirve update_control

--- src/models/Control.js
+++ src/models/Control.js
@@ -3,59 +3,69 @@ import mongoose from 'mongoose';
 const ControlSchema = new mongoose.Schema({
   numeroLeche: {
     type: String,
-    required: true,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   tipoLeche: {
     type: String,
-    required: true,
+    required: false,  // Cambiado a `false` para que sea opcional
     enum: ['MADURA', 'CALOSTRO', 'INTERMEDIA'],  // Opciones según la tabla
   },
   tipoDonacion: {
     type: String,
-    required: true,
+    required: false,  // Cambiado a `false` para que sea opcional
     enum: ['INTERNA', 'EXTERNA'],  // Opciones según la tabla
   },
   donadora: {
     type: String,
-    required: true,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   ml: {
     type: Number,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   fechaExtraccion: {
     type: String,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   horaExtraccion: {
     type: String,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   sdg: {
     type: Number,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   embalaje: {
     type: String,
     enum: ['CUMPLE', 'NO CUMPLE'],  // Opciones para embalaje
     default: 'CUMPLE',  // Valor por defecto
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   suciedad: {
     type: String,
     enum: ['CUMPLE', 'NO CUMPLE'],  // Opciones para suciedad
     default: 'CUMPLE',
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   color: {
     type: String,
     enum: ['CUMPLE', 'NO CUMPLE'],  // Opciones para color
     default: 'CUMPLE',
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   olor: {
     type: String,
     enum: ['CUMPLE', 'NO CUMPLE'],  // Opciones para olor
     default: 'CUMPLE',
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   crematocrito: {
     type: Number,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   acidezDornic: {
     type: Number,
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   observaciones: {
     type: String,
@@ -67,11 +77,12 @@ const ControlSchema = new mongoose.Schema({
       'VOLUMEN INSUFICIENTE',
     ],  // Opciones de observaciones según la tabla
     default: 'NA',  // Valor por defecto
+    required: false,  // Cambiado a `false` para que sea opcional
   },
   donanteId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Donante',  // Relación uno a uno con Donante
-    required: true,
+    required: true,  // Este campo sigue siendo obligatorio para mantener la relación
   },
   createdAt: {
     type: Date,
